ae645309875d2031ed197d47ae663f77b660b088
DocPrior <Sara Prior>
2017-05-16 14:13:18 -0400

    created separate arrays and hashes

--- exercises.rb
+++ exercises.rb
@@ -1,3 +1,28 @@
-fav_things = ["red", "purple", 29, 32, 27, "heads", "heads", "Noisuf-x",  "Crystal Method", "Juno Reactor", :red, :purple]
+fav_colours = ["red", "purple", "green"]
+people = [29, 32, 27]
+coin_flips = ["heads", "tails", "tails", "heads", "tails"]
+artist =  ["Noisuf-x",  "Crystal Method", "Juno Reactor"]
+fav_colours_2 = [:red, :purple, :green]
 
-info_box = { gibberish:  "ununderstandable speech", blank: "containing nothing", brain: "main organ of the centeral nervous system", StarWars: 1977, MadMaxFuryRoad: 2015, StarTreak: 2016, Toronto: 2600000, NewYork: 8491000, LittleRock: 197706, Penny: 29, Kate: 32, William: 27}
+words = {
+  gibberish:  "ununderstandable speech",
+  blank: "containing nothing",
+  brain: "main organ of the centeral nervous system"
+}
+fav_movies = {
+  "Star Wars" => 1977,
+  "Mad Max Fury Road" => 2015,
+  "Star Treak" => 2016
+}
+
+cities = {
+  Toronto: 2600000,
+  NewYork: 8491000,
+  LittleRock: 197706
+}
+
+friends = {
+  Penny: 29,
+  Kate: 32,
+  William: 27
+}
