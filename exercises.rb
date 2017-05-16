cd86f93acae00de024468beed858e94d532428bd
DocPrior <Sara Prior>
2017-05-16 13:58:37 -0400

    first commit, make array and hash

---
+++ exercises.rb
@@ -0,0 +1,3 @@
+fav_things = ["red", "purple", 29, 32, 27, "heads", "heads", "Noisuf-x",  "Crystal Method", "Juno Reactor", :red, :purple]
+
+info_box = { gibberish:  "ununderstandable speech", blank: "containing nothing", brain: "main organ of the centeral nervous system", StarWars: 1977, MadMaxFuryRoad: 2015, StarTreak: 2016, Toronto: 2600000, NewYork: 8491000, LittleRock: 197706, Penny: 29, Kate: 32, William: 27}
